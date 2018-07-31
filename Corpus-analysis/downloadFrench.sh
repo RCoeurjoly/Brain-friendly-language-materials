b100702248add3b114f13b1f1844cec9fe182b1a
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2018-07-31 11:44:25 +0200

    Hack script to download only epubs and remove everything else

--- Corpus-analysis/downloadFrench.sh
+++ Corpus-analysis/downloadFrench.sh
@@ -1,3 +1,5 @@
-for i in {1..11000}; do
-    wget -O $i.epub bibliothequenumerique.tv5monde.com/download/epub/$i
-done
+wget -r -X /download/pdf,/skin,/media,/livres,/auteur,/js,/auteurs,/css bibliothequenumerique.tv5monde.com
+rm -rf bibliothequenumerique.tv5monde.com/livre
+rm bibliothequenumerique.tv5monde.com/livres
+rm bibliothequenumerique.tv5monde.com/auteurs
+rm bibliothequenumerique.tv5monde.com/*html*
